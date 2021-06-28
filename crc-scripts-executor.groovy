60d90002b5db18a3a75400856e343687e2f0c986
Lukasz Nowakowski <lukasz.nowakowski@sap.com>
2021-06-28 09:32:55 +0200

    Create remote scripts executor

---
+++ crc-scripts-executor.groovy
@@ -0,0 +1,27 @@
+import org.apache.commons.io.*
+import org.apache.commons.lang.StringUtils
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+
+
+def baseUrl = 'raw.githubusercontent.com/sap-onestrike/crc-scripts/develop/scripts'
+
+def includeScripts = []
+def scriptsList = StringUtils.split(IOUtils.toString(new URL("https://${baseUrl}/list.txt"), 'utf-8'), '\n')
+def js = new JsonSlurper()
+def results = []
+
+scriptsList*.trim().each { s ->
+  if (s && (includeScripts.isEmpty() || s in includeScripts)) {
+    def sc = remoteScriptsRepository.lookupScript('https', "${baseUrl}/${s}")
+    def name = FilenameUtils.getBaseName(s)
+    
+    println "running '${name}'..."
+    
+    def res = scriptingLanguagesExecutor.executeScript(sc.engineName, sc.content, false)
+    
+    results << [ 'name': name, 'ok': !((res.stacktraceText?.trim()) as boolean), 'result': js.parseText(res.executionResult), 'error': res.stacktraceText ]
+  }
+}
+
+JsonOutput.prettyPrint(JsonOutput.toJson(results))
