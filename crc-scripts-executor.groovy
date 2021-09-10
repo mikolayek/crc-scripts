ec193c35a4999a8cf49e2e8d603e1f5c53d5d6c7
Pawel Wolanski <pawel.wolanski@sap.com>
2021-09-10 13:28:10 +0200

    added documentation, updated spliting functionality, which did not work properly

--- crc-scripts-executor.groovy
+++ crc-scripts-executor.groovy
@@ -3,11 +3,13 @@ import org.apache.commons.lang.StringUtils
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
 
+// dependencies are available in SAP Commerce environment
+// please run script in hac groovy console
 
 def baseUrl = 'raw.githubusercontent.com/sap-onestrike/crc-scripts/main/scripts'
 
 def includeScripts = []
-def scriptsList = StringUtils.split(IOUtils.toString(new URL("https://${baseUrl}/list.txt?${System.currentTimeMillis()}"), 'utf-8'), '\n')
+def scriptsList = "https://${baseUrl}/list.txt?${System.currentTimeMillis()}".toURL().text.split("\r\n*")
 def js = new JsonSlurper()
 def results = []
 
@@ -25,5 +27,4 @@ scriptsList*.trim().each { s ->
     results << [ 'name': name, 'ok': !((res.stacktraceText?.trim()) as boolean), 'result': js.parseText(res.executionResult), 'error': res.stacktraceText ]
   }
 }
-
 JsonOutput.prettyPrint(JsonOutput.toJson(results))
